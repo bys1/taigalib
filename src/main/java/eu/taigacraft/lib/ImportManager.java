fcf9a38874a6acee9375859eae4e62e5cc733050
bys1 <bjorn.schonrock@live.nl>
2016-08-09 11:34:18 +0200

    Add ImportManager

---
+++ src/main/java/eu/taigacraft/lib/ImportManager.java
@@ -0,0 +1,9 @@
+package eu.taigacraft.lib;
+
+import eu.taigacraft.importer.permissions.PermissionsImporter;
+
+public final class ImportManager {
+	
+	public final PermissionsImporter importer = PermissionsImporter.get();
+	
+}
