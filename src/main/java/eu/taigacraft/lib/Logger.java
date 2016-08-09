b79fbd644232c49e81b6b0173a01ffc7b1908d51
bys1 <bjorn.schonrock@live.nl>
2016-08-09 11:33:11 +0200

    Add Logger

---
+++ src/main/java/eu/taigacraft/lib/Logger.java
@@ -0,0 +1,44 @@
+package eu.taigacraft.lib;
+
+public class Logger {
+	
+	public enum Level {
+		INFO,NOTICE,DEBUG;
+	}
+
+	protected TaigaPlugin plugin;
+	protected Level level;
+	
+	public Logger(TaigaPlugin plugin) {
+		this.plugin = plugin;
+	}
+	
+	public void info(String message) {
+		plugin.getLogger().info(message);
+	}
+	
+	public void warn(String message) {
+		plugin.getLogger().log(java.util.logging.Level.WARNING,message);
+	}
+	
+	public void error(String message) {
+		plugin.getLogger().log(java.util.logging.Level.SEVERE,message);
+	}
+	
+	public void error(String message, Throwable exception) {
+		plugin.getLogger().log(java.util.logging.Level.SEVERE,message,exception);
+	}
+	
+	public void notice(String message) {
+		if ( (this.level == Level.NOTICE) || (this.level == Level.DEBUG) ) {
+			plugin.getLogger().info("[NOTICE] " + message);
+		}
+	}
+	
+	public void debug(String message) {
+		if (this.level == Level.DEBUG) {
+			plugin.getLogger().info("[DEBUG] " + message);
+		}
+	}
+	
+}
